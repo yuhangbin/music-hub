8983458a94a026e822e50f77535e892141afd210
yuhangbin <yuhangbinccc@gmail.com>
2023-06-11 12:36:32 +0800

    feat: add application layer and infrastructure layer. change service layer to domain layer

---
+++ application/src/test/java/com/cboy/application/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cboy.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
