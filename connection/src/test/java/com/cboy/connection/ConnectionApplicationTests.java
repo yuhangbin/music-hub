e09775ab4a0b29d6a7a6c0ee5b94bcb0d0515cdc
yuhangbin <yuhangbinccc@gmail.com>
2023-06-13 23:38:24 +0800

    feat: add connection module

---
+++ connection/src/test/java/com/cboy/connection/ConnectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cboy.connection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
